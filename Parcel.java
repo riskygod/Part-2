3b762f372f5fcb009992d6edaa0e20eb145a8e2f
riskygod <121676710+riskygod@users.noreply.github.com>
2025-01-07 23:53:58 +0000

    Update Parcel.java

--- Parcel.java
+++ Parcel.java
@@ -1,22 +1,31 @@
-import java.util.Arrays;
-
 public class Parcel {
     private String id;
-    private int[] dimensions;
+    private double length;
+    private double width;
+    private double height;
     private double weight;
+    private int daysInWarehouse;
 
-    public Parcel(String id, int[] dimensions, double weight) {
+    public Parcel(String id, double length, double width, double height, double weight, int daysInWarehouse) {
         this.id = id;
-        this.dimensions = dimensions;
+        this.length = length;
+        this.width = width;
+        this.height = height;
         this.weight = weight;
+        this.daysInWarehouse = daysInWarehouse;
     }
 
+    // Getters
     public String getId() { return id; }
-    public int[] getDimensions() { return dimensions; }
+    public double getLength() { return length; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
     public double getWeight() { return weight; }
+    public int getDaysInWarehouse() { return daysInWarehouse; }
 
     @Override
     public String toString() {
-        return "Parcel{id='" + id + "', dimensions=" + Arrays.toString(dimensions) + ", weight=" + weight + "}";
+        return String.format("Parcel{id='%s', dimensions=%.2fx%.2fx%.2f, weight=%.2f, days=%d}",
+                id, length, width, height, weight, daysInWarehouse);
     }
 }
