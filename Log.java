68d12c90a2f279bc8dfe104340d7f7580d18158b
riskygod <121676710+riskygod@users.noreply.github.com>
2025-01-07 23:53:17 +0000

    Update Log.java

--- Log.java
+++ Log.java
@@ -1,9 +1,13 @@
+import java.io.*;
+import java.util.*;
+
 public class Log {
     private static Log instance;
-    private StringBuffer logBuffer;
+    private List<String> events;
 
+    // Private constructor for Singleton pattern
     private Log() {
-        logBuffer = new StringBuffer();
+        events = new ArrayList<>();
     }
 
     public static Log getInstance() {
@@ -14,10 +18,22 @@ public class Log {
     }
 
     public void addEvent(String event) {
-        logBuffer.append(event).append("\n");
+        events.add(event);
+    }
+
+    public String getLogData() {
+        StringBuilder logData = new StringBuilder();
+        for (String event : events) {
+            logData.append(event).append("\n");
+        }
+        return logData.toString();
     }
 
     public void writeToFile(String filename) {
-        // Implementation to write logBuffer to file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(getLogData());
+        } catch (IOException e) {
+            System.err.println("Error writing log to file: " + e.getMessage());
+        }
     }
 }
